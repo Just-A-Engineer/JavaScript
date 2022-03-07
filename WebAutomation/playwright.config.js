7813b2d8110c86338fad837148b0670289798dc4
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2022-03-07 16:30:49 -0700

    Added files with updates
    Added "new" file (firstTest.spec.js) with updated require function, instead of previous import {} from {}. Also added a config file, because I was running into timeout errors.

---
+++ WebAutomation/playwright.config.js
@@ -0,0 +1,9 @@
+const config = {
+    use: {
+      headless: false,
+      viewport: { width: 1080, height: 720 },
+      globalTimeout: 200000
+    },
+  };
+  
+  module.exports = config;
---
+++ WebAutomation/tests/firstTest.spec.js
@@ -0,0 +1,14 @@
+const{ test, expect } = require("@playwright/test");
+
+test("Beginner Test", async({page}) => {
+    await page.goto("https://example.com/")
+    const pageTitle = await page.locator("h1")
+    await expect(pageTitle).toContainText("Example Domain")
+})
+
+test("Click On Element", async ({page}) => {
+    await page.goto("https://scrapethissite.com/");
+    await page.click("text = Explore Sandbox")
+    const pageTitle = await page.locator("h1")
+    await expect(pageTitle).toContainText("Web Scraping Sandbox")
+})
